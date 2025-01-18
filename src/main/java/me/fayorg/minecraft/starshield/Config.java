9166fca903bbe91c1149f63c7b27ea09920f2fea
Fayorg <ebr@aketechnology.com>
2025-01-18 11:42:49 -0800

    add: initial commit

---
+++ src/main/java/me/fayorg/minecraft/starshield/Config.java
@@ -0,0 +1,36 @@
+package me.fayorg.minecraft.starshield;
+
+import net.minecraft.core.registries.BuiltInRegistries;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.Item;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.common.EventBusSubscriber;
+import net.neoforged.fml.event.config.ModConfigEvent;
+import net.neoforged.neoforge.common.ModConfigSpec;
+
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+@EventBusSubscriber(modid = Starshield.MODID, bus = EventBusSubscriber.Bus.MOD)
+public class Config {
+    private static final ModConfigSpec.Builder BUILDER = new ModConfigSpec.Builder();
+
+    // config entries
+
+    static final ModConfigSpec SPEC = BUILDER.build();
+
+    /*private static boolean validateItemName(final Object obj) {
+        return obj instanceof String itemName && BuiltInRegistries.ITEM.containsKey(ResourceLocation.parse(itemName));
+    }*/
+
+    /*@SubscribeEvent
+    static void onLoad(final ModConfigEvent event) {
+        logDirtBlock = LOG_DIRT_BLOCK.get();
+        magicNumber = MAGIC_NUMBER.get();
+        magicNumberIntroduction = MAGIC_NUMBER_INTRODUCTION.get();
+
+        // convert the list of strings into a set of items
+        items = ITEM_STRINGS.get().stream().map(itemName -> BuiltInRegistries.ITEM.get(ResourceLocation.parse(itemName))).collect(Collectors.toSet());
+    }*/
+}
